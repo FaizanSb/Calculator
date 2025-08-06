adab9c4dc6bf04d176ae6d25d553fe30714079a7
Muhammad Faizan <mf1341876@gmail.com>
2025-08-06 18:42:49 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -10,36 +10,43 @@ buttons.forEach(btn => {
     btn.addEventListener('click', (e) => {
         let val = e.target.value;
         console.log(val)
-       
-        
 
-        if(val==="C"){
+
+
+        if (val === "C") {
             screen.innerHTML = '';
         }
-        else if(val==="=") {
-            screen.innerHTML = eval(screen.innerHTML);
-
-            if(screen.innerHTML==="infinity"){
-                screen.innerHTML = "Error";
+        else if (val === "=") {
+            let result = eval(screen.innerHTML);
+            try {
+                if (result === Infinity || result === -Infinity || isNaN(result)) {
+                    screen.innerHTML = 'Error';
+                }
+                else {
+                    screen.innerHTML = result;
+                }
+            } catch (e) {
+                screen.innerHTML = 'Error';
             }
 
         }
-        else if(val==="del") {
-            screen.innerHTML = screen.innerHTML.slice(0,-1);
+
+        else if (val === "del") {
+            screen.innerHTML = screen.innerHTML.slice(0, -1);
         }
-        else if(val==="%") {
-            screen.innerHTML = screen.innerHTML/100;
+        else if (val === "%") {
+            screen.innerHTML = screen.innerHTML / 100;
         }
-        else if(val==="+/-") {
+        else if (val === "+/-") {
             screen.innerHTML = -screen.innerHTML;
         }
-       
-        else{
-            screen.innerHTML = screen.innerHTML+val;
-            console.log("Length",screen.innerHTML.length)
-    
-        }   
- 
+
+        else {
+            screen.innerHTML = screen.innerHTML + val;
+
+
+        }
+
     })
 });
 
