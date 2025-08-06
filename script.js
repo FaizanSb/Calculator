61ae022ed6374f914dd26e26437efb9103a0941f
Muhammad Faizan <mf1341876@gmail.com>
2025-08-06 17:58:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+console.log("This is console file")
+
+
+let buttons = document.querySelectorAll('.buttons .btn');
+console.log(buttons)
+let screen = document.getElementById('Scr');
+console.log(screen)
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        let val = e.target.value;
+        console.log(val)
+       
+        
+
+        if(val==="C"){
+            screen.innerHTML = '';
+        }
+        else if(val==="=") {
+            screen.innerHTML = eval(screen.innerHTML);
+
+            if(screen.innerHTML==="infinity"){
+                screen.innerHTML = "Error";
+            }
+
+        }
+        else if(val==="del") {
+            screen.innerHTML = screen.innerHTML.slice(0,-1);
+        }
+        else if(val==="%") {
+            screen.innerHTML = screen.innerHTML/100;
+        }
+        else if(val==="+/-") {
+            screen.innerHTML = -screen.innerHTML;
+        }
+       
+        else{
+            screen.innerHTML = screen.innerHTML+val;
+            console.log("Length",screen.innerHTML.length)
+    
+        }   
+ 
+    })
+});
+
+
+
